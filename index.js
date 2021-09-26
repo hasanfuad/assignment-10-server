c624969f177d020529e4e0081e515c33427e1f56
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-27 01:04:29 +0600

    fixed env

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shttn.mongodb.net/foodBazar?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shttn.mongodb.net/${process.env.DB_USER}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
